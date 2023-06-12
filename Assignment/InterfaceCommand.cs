79f2a46d350379c25d01e6458cff697af7d4bf18
simranjeetkaur2001 <130950892+simranjeetkaur2001@users.noreply.github.com>
2023-06-12 01:19:53 +0000

    completed interface commands

--- Assignment/InterfaceCommand.cs
+++ Assignment/InterfaceCommand.cs
@@ -1,6 +1,84 @@
-﻿namespace Assignment.InterfaceCommand;
-
-interface RobotCommand
+﻿namespace Assignment.InterfaceCommand
 {
-    void Run(Robot robot); // Interfaces are public and abstract by default.
+    public interface RobotCommand
+    {
+        void Run(Robot robot);
+    }
+
+    public class OffCommand : RobotCommand
+    {
+        public void Run(Robot robot)
+        {
+            robot.IsPowered = false;
+        }
+    }
+
+    public class OnCommand : RobotCommand
+    {
+        public void Run(Robot robot)
+        {
+            robot.IsPowered = true;
+        }
+    }
+
+    public class WestCommand : RobotCommand
+    {
+        public void Run(Robot robot)
+        {
+            if (robot.IsPowered)
+            {
+                robot.X--;
+            }
+        }
+    }
+
+    public class EastCommand : RobotCommand
+    {
+        public void Run(Robot robot)
+        {
+            if (robot.IsPowered)
+            {
+                robot.X++;
+            }
+        }
+    }
+
+    public class SouthCommand : RobotCommand
+    {
+        public void Run(Robot robot)
+        {
+            if (robot.IsPowered)
+            {
+                robot.Y--;
+            }
+        }
+    }
+
+
+    public class RebootCommand : RobotCommand
+    {
+        public void Run(Robot robot)
+        {
+            if (robot.IsPowered)
+            {
+                robot.Y = 0;
+                robot.X = 0;
+                robot.IsPowered = false;
+            }
+        }
+    }
+
+
+    public class NorthCommand : RobotCommand
+    {
+        public void Run(Robot robot)
+        {
+            if (robot.IsPowered)
+            {
+                robot.Y++;
+            }
+        }
+    }
+
+
 }
--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,10 +1,66 @@
-﻿namespace Assignment
+﻿using Assignment.InterfaceCommand;
+namespace Assignment
 {
-    static class Program
+    public class RobotTester
     {
-        static void Main()
+        public void TestRobotCommands()
         {
-            // Run your RobotTester class here -> RobotTester.TestRobot()
+            Robot robot = new Robot();
+            string commandString;
+
+            while (true)
+            {
+                Console.WriteLine("Enter a command ('exit' to quit):");
+                commandString = Console.ReadLine();
+
+                if (commandString.Equals("exit", StringComparison.OrdinalIgnoreCase))
+                    break;
+
+                IRobotCommand command = ConvertToCommand(commandString);
+                if (command != null)
+                {
+                    robot.LoadCommand(command);
+                    robot.Run();
+                    Console.WriteLine(robot.ToString());
+                }
+                else
+                {
+                    Console.WriteLine("Invalid command. Please try again.");
+                }
+            }
+        }
+
+        private IRobotCommand ConvertToCommand(string commandString)
+        {
+            switch (commandString.ToLower())
+            {
+                case "on":
+                    return new OnCommand();
+                case "off":
+                    return new OffCommand();
+                case "north":
+                    return new NorthCommand();
+                case "south":
+                    return new SouthCommand();
+                case "east":
+                    return new EastCommand();
+                case "west":
+                    return new WestCommand();
+                default:
+                    return null;
+            }
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            RobotTester robotTester = new RobotTester();
+            robotTester.TestRobotCommands();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
     }
 }
